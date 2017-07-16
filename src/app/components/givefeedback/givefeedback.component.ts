965fea92283b36e8d06190e7b7fccb5cb4a5c592
bulenthacioglu <bulent.hacioglu@hotmail.com>
2017-07-16 14:07:03 +0200

    Feedback mechanism fix

--- src/app/components/givefeedback/givefeedback.component.ts
+++ src/app/components/givefeedback/givefeedback.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/user';
+import { Offer } from '../../models/offer';
+import { Feedback } from '../../models/feedback';
 @Component({
   selector: 'app-givefeedback',
   templateUrl: './givefeedback.component.html',
   styleUrls: ['./givefeedback.component.css']
 })
 export class GivefeedbackComponent implements OnInit {
-
+private rate = 0;
+private a;
+feedback:Feedback;
   constructor() { }
 
   ngOnInit() {
+    
+
+  }
+  createFeedback(event){
 
   }
 
 }
 
-export class NgbdRatingBasic {
+/*export class NgbdRatingBasic {
   currentRate = 8;
-}
+  print(){
+    console.log(this.currentRate);
+  }
+}*/
