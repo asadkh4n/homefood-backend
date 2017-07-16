22529065cb01d95e4d465c2774ae30c2df375d4a
bulenthacioglu <bulent.hacioglu@hotmail.com>
2017-07-16 18:41:06 +0200

    give feedback mechanism completed

--- src/app/components/givefeedback/givefeedback.component.ts
+++ src/app/components/givefeedback/givefeedback.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { Offer } from '../../models/offer';
 import { Feedback } from '../../models/feedback';
+import { FeedbackService } from '../../services/feedback.service';
+import { OrderService } from '../../services/order.service';
 @Component({
   selector: 'app-givefeedback',
   templateUrl: './givefeedback.component.html',
@@ -9,16 +11,36 @@ import { Feedback } from '../../models/feedback';
 })
 export class GivefeedbackComponent implements OnInit {
 private rate = 0;
-private a;
-feedback:Feedback;
-  constructor() { }
+private feedback = new Feedback;
+private order;
+private user_id = "d";
+private offer_id;
+constructor(private feedbackService: FeedbackService,
+            private orderService: OrderService
+              ) { }
 
   ngOnInit() {
     
-
   }
   createFeedback(event){
-
+    this.getOrder();
+    this.feedback.user = this.user_id;
+    this.feedback.offer = this.offer_id;
+    this.feedbackService.createFeedback(this.feedback)
+          .subscribe( data => {
+            var createdFeedback = JSON.parse(JSON.stringify(data));
+    });
+  }
+  getOrder(){
+    var url = location.pathname;
+    var res = url.split("/");
+    var id = res[2];
+    this.orderService.getOrder(id).subscribe(order =>{
+      this.order = order;
+      this.user_id = order.user;
+      this.offer_id = order.offer;
+      console.log(this.offer_id);
+    })
   }
 
 }
