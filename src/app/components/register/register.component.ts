ac3cd7aea8ec9c03706b1771073cb4937fc081a2
bulenthacioglu <bulent.hacioglu@hotmail.com>
2017-07-18 21:49:41 +0200

    bug fixes

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ import { UserService } from '../../services/user.service';
 })
 export class RegisterComponent implements OnInit {
   user: User = new User();
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router, private authenticationService:AuthenticationService) { }
 
   ngOnInit() {
   }
@@ -20,6 +21,8 @@ export class RegisterComponent implements OnInit {
     this.userService.createUser(this.user)
       .subscribe( success => {
         if (success) {
+          this.authenticationService.login(this.user.username, this.user.password)
+           .subscribe();
           this.router.navigate(['/home']);
         } else {
           alert('Error while creating offer!');
